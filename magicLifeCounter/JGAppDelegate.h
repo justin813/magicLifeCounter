987b69189ad232566894fd3b13ec46a635590dcc
Justin Gelalia <justingelalia@Justins-MacBook-Pro.local>
2014-10-09 13:33:05 -0400

    Initial Commit

---
+++ magicLifeCounter/JGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAppDelegate.h
+//  magicLifeCounter
+//
+//  Created by Justin Gelalia on 10/9/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ magicLifeCounter/JGViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGViewController.h
+//  magicLifeCounter
+//
+//  Created by Justin Gelalia on 10/9/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGViewController : UIViewController
+
+@end
