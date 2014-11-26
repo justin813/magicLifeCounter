92441cf52cfddbcb8380c25e70b02e24c460993b
justin813 <justingelalia@gmail.com>
2014-11-26 01:59:34 -0500

    App

--- magicLifeCounter/JGViewController.h
+++ magicLifeCounter/JGViewController.h
@@ -9,5 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface JGViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *player1;
+@property (strong, nonatomic) IBOutlet UILabel *player2;
+@property (strong, nonatomic) IBOutlet UILabel *player1Health;
+@property (strong, nonatomic) IBOutlet UILabel *player2Health;
+- (IBAction)player1Increace:(UIButton *)sender;
+- (IBAction)player1Decreace:(UIButton *)sender;
+- (IBAction)player2Increace:(UIButton *)sender;
+- (IBAction)player2Decreace:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UITextField *p1Name;
+- (IBAction)resetButton:(UIButton *)sender;
+@end
+@interface ViewController : UIViewController <UITextFieldDelegate> {
+}
 
 @end
--- magicLifeCounter/JGViewController.m
+++ magicLifeCounter/JGViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.p1Name.delegate = self;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +26,49 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)player1Increace:(UIButton *)sender {
+    
+    int player1Hp = [self.player1Health.text intValue];
+    player1Hp++;
+    self.player1Health.text = [NSString stringWithFormat:@"%i", player1Hp];
+}
+
+- (IBAction)player1Decreace:(UIButton *)sender
+{
+    int player1Hp = [self.player1Health.text intValue];
+    player1Hp--;
+    self.player1Health.text = [NSString stringWithFormat:@"%i", player1Hp];
+}
+
+- (IBAction)player2Increace:(UIButton *)sender
+{
+    int player2Hp = [self.player2Health.text intValue];
+    player2Hp++;
+    self.player2Health.text = [NSString stringWithFormat:@"%i", player2Hp];
+    
+}
+
+- (IBAction)player2Decreace:(UIButton *)sender
+{
+    int player2Hp = [self.player2Health.text intValue];
+    player2Hp--;
+    self.player2Health.text = [NSString stringWithFormat:@"%i", player2Hp];
+}
+
+- (IBAction)resetButton:(UIButton *)sender
+{
+    self.player1Health.text = [NSString stringWithFormat:@"%i", 20];
+    self.player2Health.text = [NSString stringWithFormat:@"%i", 20];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+
+
 
 @end
